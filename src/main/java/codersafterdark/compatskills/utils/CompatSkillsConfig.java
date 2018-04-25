301dfd722d5c0ea5a69396d3f0bbc6c3330d4ee2
Lanse <simon_stalnabb@hotmail.com>
2018-04-25 20:01:29 +0200

    Added Config options to require Shifting for TOP and Hwyla Support

--- src/main/java/codersafterdark/compatskills/utils/CompatSkillsConfig.java
+++ src/main/java/codersafterdark/compatskills/utils/CompatSkillsConfig.java
@@ -1,4 +1,26 @@
 package codersafterdark.compatskills.utils;
 
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.Comment;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
 public class CompatSkillsConfig {
+    public static void postInit(FMLPostInitializationEvent event){
+    }
+
+    @Config(modid = CompatSkillConstants.MOD_ID)
+    public static class StargazerConfigs {
+        public static Hwyla hwyla;
+        public static TOP top;
+
+        public static class Hwyla {
+            @Comment("Should Hwyla Requirements Support require Shifting?")
+            public static boolean HwylaShifting = false;
+        }
+
+        public static class TOP {
+            @Comment("Should TOP Requirements Support require Shifting?")
+            public static boolean TOPShifting = false;
+        }
+    }
 }
