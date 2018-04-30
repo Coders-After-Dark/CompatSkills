4e12ea72faeec35764a8646b6b4fbde04df8192e
Lanse <simon_stalnabb@hotmail.com>
2018-05-01 00:31:23 +0200

    Version Bump

--- src/main/java/codersafterdark/compatskills/utils/CompatSkillConstants.java
+++ src/main/java/codersafterdark/compatskills/utils/CompatSkillConstants.java
@@ -3,7 +3,7 @@ package codersafterdark.compatskills.utils;
 public class CompatSkillConstants {
     public static final String MOD_ID = "compatskills";
     public static final String MOD_NAME = "CompatSkills";
-    public static final String VERSION = "1.12.2-1.2.1";
+    public static final String VERSION = "1.12.2-1.4.0";
     public static final String DEPENDENCIES = "required-after:reskillable";
     public static final String MCVER = "1.12,";
 
