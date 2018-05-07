39dc053ec03b1f928ca59899c192a890ced3da86
Lanse <simon_stalnabb@hotmail.com>
2018-05-07 19:20:46 +0200

    Added Dimesion Requirement and Inverted Variant

---
+++ src/main/java/codersafterdark/compatskills/common/compats/minecraft/dimension/dimensionrequirement/DimensionRequirement.java
@@ -0,0 +1,38 @@
+package codersafterdark.compatskills.common.compats.minecraft.dimension.dimensionrequirement;
+
+import codersafterdark.reskillable.api.data.PlayerData;
+import codersafterdark.reskillable.api.requirement.Requirement;
+import codersafterdark.reskillable.api.requirement.RequirementComparision;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextFormatting;
+
+public class DimensionRequirement extends Requirement {
+    private int dimension;
+
+    public DimensionRequirement(int dimension){
+        this.dimension = dimension;
+    }
+
+    @Override
+    public boolean achievedByPlayer(EntityPlayer entityPlayerMP) {
+        return entityPlayerMP.dimension == dimension;
+    }
+
+    @Override
+    public String getToolTip(PlayerData data) {
+        TextFormatting color = TextFormatting.GREEN;
+        EntityPlayer player = data.playerWR.get();
+        if (player != null){
+            if (player.dimension != dimension){
+                color = TextFormatting.RED;
+            }
+        }
+        return TextFormatting.GRAY + " - " + TextFormatting.LIGHT_PURPLE + I18n.format("compatskills.misc.dimensionFormat", color, dimension);
+    }
+
+    @Override
+    public RequirementComparision matches(Requirement other) {
+        return other instanceof DimensionRequirement && dimension == (((DimensionRequirement) other).dimension) ? RequirementComparision.EQUAL_TO : RequirementComparision.NOT_EQUAL;
+    }
+}
