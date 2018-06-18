85dccfd62d8362ca38150241f27e9bd70b5844fe
pupnewfster <richard@freimer.com>
2018-06-17 20:10:26 -0400

    Fixed server crash due to sidedness

--- src/main/java/codersafterdark/compatskills/common/compats/projecte/ProjectECompatHandler.java
+++ src/main/java/codersafterdark/compatskills/common/compats/projecte/ProjectECompatHandler.java
@@ -5,13 +5,16 @@ import moze_intel.projecte.gameObjs.gui.GUICondenser;
 import moze_intel.projecte.gameObjs.gui.GUICondenserMK2;
 import moze_intel.projecte.gameObjs.gui.GUITransmutation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class ProjectECompatHandler {
-    public static void setup() {
+    public static void setup(Side side) {
         TransmutationLockHandler handler = new TransmutationLockHandler();
         MinecraftForge.EVENT_BUS.register(handler);
-        ToolTipHandler.addTooltipInjector(GUITransmutation.class, handler::transmutationTooltip);
-        ToolTipHandler.addTooltipInjector(GUICondenser.class, handler::condenserTooltip);
-        ToolTipHandler.addTooltipInjector(GUICondenserMK2.class, handler::condenserTooltip); //The class does not extend the other one so it is also needed
+        if (side.isClient()) {
+            ToolTipHandler.addTooltipInjector(GUITransmutation.class, handler::transmutationTooltip);
+            ToolTipHandler.addTooltipInjector(GUICondenser.class, handler::condenserTooltip);
+            ToolTipHandler.addTooltipInjector(GUICondenserMK2.class, handler::condenserTooltip); //The class does not extend the other one so it is also needed
+        }
     }
 }
