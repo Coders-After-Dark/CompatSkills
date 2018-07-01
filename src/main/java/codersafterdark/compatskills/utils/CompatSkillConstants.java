613e575af06362819777b883ccc696729522417b
pupnewfster <richard@freimer.com>
2018-06-30 22:55:15 -0400

    Change it so that it uses the projecte lib off of curse and has min version for projecte compat

--- src/main/java/codersafterdark/compatskills/utils/CompatSkillConstants.java
+++ src/main/java/codersafterdark/compatskills/utils/CompatSkillConstants.java
@@ -7,7 +7,7 @@ public class CompatSkillConstants {
     public static final String MOD_NAME = "CompatSkills";
     public static final String VERSION = "1.12.2-1.9.0";
     public static final String DEPENDENCIES = "required-after:reskillable@[1.8.0,);after:baubles;after:bloodmagic;after:gamestages;after:immersiveengineering;" +
-            "after:magneticraft;after:tconstruct;after:theoneprobe;after:projecte;";//TODO: Should it include a min version of projecte (after next update if pull is merged)
+            "after:magneticraft;after:tconstruct;after:theoneprobe;after:projecte@[1.3.1B,);";
     public static final String MCVER = "1.12,";
 
     //Proxy Constants
