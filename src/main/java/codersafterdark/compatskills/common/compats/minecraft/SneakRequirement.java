628da62c6840cdf58074198cbd15687a4f8c7d87
pupnewfster <richard@freimer.com>
2018-10-12 18:08:55 -0400

    Fix coloring of sneaking requirement

--- src/main/java/codersafterdark/compatskills/common/compats/minecraft/SneakRequirement.java
+++ src/main/java/codersafterdark/compatskills/common/compats/minecraft/SneakRequirement.java
@@ -9,13 +9,14 @@ import net.minecraft.util.text.TextFormatting;
 
 public class SneakRequirement extends Requirement implements UncacheableRequirement {
     public SneakRequirement() {
-        this.tooltip = TextFormatting.GRAY + " - " + new TextComponentTranslation("compatskills.misc.sneakFormat").getUnformattedComponentText();
+        this.tooltip = TextFormatting.GRAY + " - " + new TextComponentTranslation("compatskills.misc.sneakFormat", "%s").getUnformattedComponentText();
     }
 
     @Override
     public boolean achievedByPlayer(EntityPlayer entityPlayerMP) {
         return entityPlayerMP.isSneaking();
     }
+
     @Override
     public RequirementComparision matches(Requirement other) {
         return equals(other) ? RequirementComparision.EQUAL_TO : RequirementComparision.NOT_EQUAL;
