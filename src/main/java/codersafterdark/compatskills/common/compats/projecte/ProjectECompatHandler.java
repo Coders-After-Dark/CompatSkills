9ac5feabd5e8219b170ff2db96de952acfb7a852
pupnewfster <richard@freimer.com>
2019-02-07 16:25:30 -0500

    Added EMC warning message to Project EX's Personal Link. Also deduplicated some code

--- src/main/java/codersafterdark/compatskills/common/compats/projecte/ProjectECompatHandler.java
+++ src/main/java/codersafterdark/compatskills/common/compats/projecte/ProjectECompatHandler.java
@@ -2,10 +2,12 @@ package codersafterdark.compatskills.common.compats.projecte;
 
 import codersafterdark.compatskills.utils.CompatModuleBase;
 import codersafterdark.reskillable.base.ToolTipHandler;
+import com.latmod.mods.projectex.gui.GuiLink;
 import moze_intel.projecte.gameObjs.gui.GUICondenser;
 import moze_intel.projecte.gameObjs.gui.GUICondenserMK2;
 import moze_intel.projecte.gameObjs.gui.GUITransmutation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Loader;
 
 public class ProjectECompatHandler extends CompatModuleBase {
     public static boolean ENABLED;
@@ -22,6 +24,11 @@ public class ProjectECompatHandler extends CompatModuleBase {
     public void clientPreInit() {
         ToolTipHandler.addTooltipInjector(GUITransmutation.class, handler::transmutationTooltip);
         ToolTipHandler.addTooltipInjector(GUICondenser.class, handler::condenserTooltip);
-        ToolTipHandler.addTooltipInjector(GUICondenserMK2.class, handler::condenserTooltip); //The class does not extend the other one so it is also needed
+        //TODO: This probably will eventually extend GUICondenser and when that is the case remove this extra line
+        ToolTipHandler.addTooltipInjector(GUICondenserMK2.class, handler::condenserTooltip);
+
+        if (Loader.isModLoaded("projectex")) {
+            ToolTipHandler.addTooltipInjector(GuiLink.class, handler::condenserTooltip);
+        }
     }
 }
--- src/main/java/codersafterdark/compatskills/common/compats/projecte/TransmutationLockHandler.java
+++ src/main/java/codersafterdark/compatskills/common/compats/projecte/TransmutationLockHandler.java
@@ -76,24 +76,15 @@ public class TransmutationLockHandler {
 
     //TODO should this have sideonly annotation
     public List<String> transmutationTooltip(ToolTipHandler.ToolTipInfo info) {
-        List<String> toolTip = new ArrayList<>();
-        PlayerData data = info.getData();
-        RequirementHolder holder = itemRequirements(info.getItem());
-        if (!holder.equals(LevelLockHandler.EMPTY_LOCK) && !data.matchStats(holder)) {
-            long cost = ProjectEAPI.getEMCProxy().getValue(info.getItem());
-            RequirementHolder emcHolder = LevelLockHandler.getLockByFuzzyKey(new EMCLockKey(cost));
-            if (!emcHolder.equals(LevelLockHandler.EMPTY_LOCK)) {
-                emcHolder.getRequirements().stream().map(requirement -> requirement.getToolTip(data)).forEach(toolTip::add);
-            }
-            if (cost > 0) {
-                toolTip.add(TextFormatting.DARK_RED + new TextComponentTranslation("compatskills.projecte.transmutationWarning").getUnformattedComponentText());
-            }
-        }
-        return toolTip;
+        return emcTooltip(info, TextFormatting.DARK_RED + new TextComponentTranslation("compatskills.projecte.transmutationWarning").getUnformattedComponentText());
     }
 
     //TODO should this have sideonly annotation
     public List<String> condenserTooltip(ToolTipHandler.ToolTipInfo info) {
+        return emcTooltip(info, TextFormatting.YELLOW + new TextComponentTranslation("compatskills.projecte.condenserWarning").getUnformattedComponentText());
+    }
+
+    private List<String> emcTooltip(ToolTipHandler.ToolTipInfo info, String message) {
         List<String> toolTip = new ArrayList<>();
         PlayerData data = info.getData();
         RequirementHolder holder = itemRequirements(info.getItem());
@@ -104,7 +95,7 @@ public class TransmutationLockHandler {
                 emcHolder.getRequirements().stream().map(requirement -> requirement.getToolTip(data)).forEach(toolTip::add);
             }
             if (cost > 0) {
-                toolTip.add(TextFormatting.YELLOW + new TextComponentTranslation("compatskills.projecte.condenserWarning").getUnformattedComponentText());
+                toolTip.add(message);
             }
         }
         return toolTip;
