9f4e874f6535791019839232fae0d2da439dc06a
TheidenHD <markojahn97@gmail.com>
2024-01-18 19:38:05 +0100

    Fixed some bugs and compatibility issues

--- src/main/java/codersafterdark/compatskills/common/compats/gamestages/gamestagerequirement/GameStageRequirementHandler.java
+++ src/main/java/codersafterdark/compatskills/common/compats/gamestages/gamestagerequirement/GameStageRequirementHandler.java
@@ -2,6 +2,7 @@ package codersafterdark.compatskills.common.compats.gamestages.gamestagerequirem
 
 import codersafterdark.reskillable.api.requirement.RequirementCache;
 import net.darkhax.gamestages.event.GameStageEvent;
+import net.darkhax.gamestages.event.StagesSyncedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class GameStageRequirementHandler {
@@ -12,6 +13,11 @@ public class GameStageRequirementHandler {
 
     @SubscribeEvent
     public void gameStageRemoved(GameStageEvent.Removed event) {
+        RequirementCache.invalidateCache(event.getEntityPlayer(), GameStageRequirement.class);
+    }
+
+    @SubscribeEvent
+    public void stagesSync(StagesSyncedEvent event) {
         RequirementCache.invalidateCache(event.getEntityPlayer(), GameStageRequirement.class);
     }
 }
